b2a6580309927720579d41816b6d4481d6dc0c69
Димас <52570598+GektoKot@users.noreply.github.com>
2021-06-18 21:48:13 +0200

    Added List of elements

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+
+const saveBtn = document.getElementById("save-btn")
+const text = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+// let items = []
+let items = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+
+render()
+
+
+
+saveBtn.addEventListener("click", function (){
+    if (text.value === undefined || text.value === "") {
+        return
+    }
+    items.push(text.value)
+    render()
+})
+
+function render() {
+    let listItem = ""
+
+    for(let item of items) {
+        listItem += "<li>" + item + "</li>"
+    }
+    
+    ulEl.innerHTML = listItem
+}
+
+
+// for(let item of items) {
+//     const liEl = document.createElement("li")
+//     liEl.textContent = item
+//     ulEl.append(liEl)
+// }
+
+
