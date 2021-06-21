35f187207b8e0b8dda9d662323372feca9cdd8eb
Димас <52570598+GektoKot@users.noreply.github.com>
2021-06-21 16:29:09 +0200

    localStorage handler

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const saveBtn = document.getElementById("save-btn")
 const text = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-// let items = []
-let items = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+// let items = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+
+let items = JSON.parse(localStorage.getItem("items"))
 
 render()
 
@@ -15,13 +16,18 @@ saveBtn.addEventListener("click", function () {
     if (text.value === undefined || text.value === "") {
         return
     }
+
     items.push(text.value)
+    localStorage.setItem("items", JSON.stringify(items))
+
     render()
 })
 
+
 function render() {
     let listItem = ""
-
+    items = JSON.parse(localStorage.getItem("items"))
+    
     for (let item of items) {
         listItem += `
                     <li>
